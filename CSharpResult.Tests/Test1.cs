bcc5ae0a0899ba315c60f310a80b4921631bae92
Abigail <184506684+jessthewizard@users.noreply.github.com>
2025-11-24 10:05:27 +0000

    Initial commit

---
+++ CSharpResult.Tests/Test1.cs
@@ -0,0 +1,20 @@
+﻿namespace CSharpResult.Tests;
+
+[TestClass]
+public sealed class Test1
+{
+    [TestMethod]
+    public void ResultErr_IntOkStringErr_IsErr()
+    {
+	    var x = Result<int, string>.Err("Test");
+	    Assert.AreEqual("Err(Test)", x.ToString());
+    }
+
+    [TestMethod]
+    public void ResultErr_IntOkStringErr_IsOk()
+    {
+	    var x = Result<int, string>.Ok(10);
+	    Assert.AreEqual("Ok(10)", x.ToString());
+    }
+
+}
---
+++ CSharpResult/Result.cs
@@ -0,0 +1,41 @@
+﻿public class Result<T, E> {
+	private enum ResultState {
+		Ok,
+		Err
+	}
+
+	private ResultState state; 
+	private T? okItem;
+	private E? errItem;
+
+	public static Result<T, E> Ok(T result)
+	{
+		var newContainer = new Result<T, E>();
+		newContainer.state = ResultState.Ok;
+		newContainer.okItem = result;
+		return newContainer;
+	}
+
+	public static Result<T, E> Err(E result)
+	{
+		var newContainer = new Result<T, E>();
+		newContainer.state = ResultState.Err;
+		newContainer.errItem = result;
+		return newContainer;
+	}
+
+	public bool IsOk()
+	{
+		return state == ResultState.Ok;
+	}
+
+	public bool IsErr()
+	{
+		return state == ResultState.Err;
+	}
+
+	public override string ToString()
+	{
+		return state + "(" + (this.IsOk() ? okItem : errItem) + ")";
+	}
+}
